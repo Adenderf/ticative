1796d9b839e7d0a4d77fcfc86f19b16462d80480
aldenderf <aldenderfc.fabro99@gmail.com>
2022-12-08 20:14:49 +0800

    feat: update repo
    remove data

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-
-import allReducers from './reducer';
-
-const store = createStore(allReducers);
-
-export default store;
--- upload/upload-multer.js
+++
@@ -1,6 +0,0 @@
-const multer = require('multer');
-const {storage} = require('./cloudinary')
-
-module.exports = multer({
-    storage: storage
-});
